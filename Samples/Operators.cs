b22cf61366a4571dfe8cf527e19a0ca7fe1d394e
Ahmet Yağız <ahmetyagiz000@gmail.com>
2022-09-06 19:24:31 +0300

    Create Operators.cs

---
+++ Samples/Operators.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+class Operators
+{
+    static void Main(string[] args)
+    {
+        //Assignment
+        Console.WriteLine("***** Assignment Operators *****");
+        int x = 3;
+        int y = 3;
+        y = y + 2;
+
+        Console.WriteLine(y);
+        y += 2;
+        Console.WriteLine(y);
+        y /= 1;
+        Console.WriteLine(y);
+        x *= 2;
+        Console.WriteLine(x);
+
+        //Logical Operators
+        Console.WriteLine("***** Logical Operators *****");
+
+        bool isSuccess = true;
+        bool isCompleted = false;
+
+        if (isSuccess && isCompleted)
+        {
+            Console.WriteLine("Perfect!");
+        }
+        if (isSuccess || isCompleted)
+        {
+            Console.WriteLine("Great!");
+        }
+        if (isSuccess && !isCompleted)
+        {
+            Console.WriteLine("Fine!");
+        }
+
+        //Relational Operators
+        Console.WriteLine("***** Relational Operators *****");
+
+        int a = 3;
+        int b = 4;
+
+        bool result = a < b;
+
+        Console.WriteLine(result);
+        result = a > b;
+        Console.WriteLine(result);
+        result = a >= b;
+        Console.WriteLine(result);
+        result = a <= b;
+        Console.WriteLine(result);
+        result = a == b;
+        Console.WriteLine(result);
+        result = a != b;
+        Console.WriteLine(result);
+
+        Console.WriteLine("***** Arithmetic Operators *****");
+
+        int value1 = 10;
+        int value2 = 5;
+        int result1 = value1 / value2;
+
+        Console.WriteLine(result1);
+
+        result1 = value1 * value2;
+        Console.WriteLine(result1);
+
+        result1 = value1 * value2;
+        Console.WriteLine(result1);
+
+        result1 += value1++;
+        Console.WriteLine(result1);
+
+        // % : mod
+        int result2 = 20 % 3;
+        Console.WriteLine(result2);
+    }
+}
