8e1e2267b9c8999bc0aff8ea1c999beef7f2ef67
Ahmet Yağız <ahmetyagiz000@gmail.com>
2022-09-07 19:57:20 +0300

    Create IfElseTernary.cs

---
+++ Samples/IfElseTernary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class IfElseTernary
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if (time >= 6 && time <= 11)
+            Console.WriteLine("Good Morning!");
+        else if (time <= 18)
+            Console.WriteLine("Have a good day!");
+        else
+            Console.WriteLine("Good Night!");
+
+        string sonuc = time <= 18 ? "Have a good day!" : "Good Night!";
+
+        sonuc = time >= 6 && time < 11 ? "Good Morning" : time <= 18 ? "Have a good day!" : "Good Night!";
+
+        Console.WriteLine(sonuc);
+    }
+}
