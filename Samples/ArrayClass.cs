aac3cc9b3a92d5b917f4a12dc9469f0901dd0113
Ahmet Yağız <ahmetyagiz000@gmail.com>
2022-09-15 12:38:43 +0300

    Create ArrayClass.cs

---
+++ Samples/ArrayClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class ArrayClass
+{
+    static void Main(string[] args)
+    {
+        int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+        Console.WriteLine("**** Sırasız Dizi ****");
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+        Console.WriteLine("**** Sıralı Dizi ****");
+        Array.Sort(sayiDizisi);
+
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        //Clear
+        Console.WriteLine("**** Array Clear ****");
+        Array.Clear(sayiDizisi, 2, 2);
+
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        //Reverse
+        Console.WriteLine("**** Array Reverse ****");
+        Array.Reverse(sayiDizisi);
+
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        //IndexOf
+        Console.WriteLine("**** Array IndexOf ****");
+        Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+        //Resize
+        Console.WriteLine("**** Array IndexOf ****");
+        Array.Resize<int>(ref sayiDizisi, 9);
+        sayiDizisi[8] = 99;
+
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+    }
+}
+
