3f54b2275842ecd85dd51f50766192a8a8d16eab
Ahmet Yağız <ahmetyagiz000@gmail.com>
2022-10-08 18:20:50 +0300

    Assignments - 2

---
+++ Samples/Assignments - 2/Assignment - 1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList nonPrimeNumbers = new ArrayList();
+            ArrayList primeNumbers = new ArrayList();
+
+            int primeNumbersSum = 0;
+            int nonPrimeNumbersSum = 0;
+
+            //input values
+            for (int i = 0; i < 20; i++)
+            {
+                int inputValue = 0;
+                bool formatCheck = true;
+                bool primeCheck = true;
+
+                while (formatCheck || inputValue < 0)
+                {
+                    try
+                    {
+                        Console.Write($"{i + 1}. sayıyı giriniz: ");
+                        inputValue = int.Parse(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Yanlış bir değer girdiniz");
+                        continue;
+                    }
+                    if (inputValue < 0)
+                    {
+                        Console.WriteLine("Yanlış bir değer girdiniz");
+                    }
+                    formatCheck = false;
+                }
+
+                for (int m = 2; m < inputValue; m++)
+                {
+                    if (inputValue % m == 0)
+                    {
+                        primeCheck = false;
+                        break;
+                    }
+                }
+
+                if (primeCheck == true)
+                {
+                    primeNumbersSum += inputValue;
+                    primeNumbers.Add(inputValue);
+                }
+                else
+                {
+                    nonPrimeNumbersSum += inputValue;
+                    nonPrimeNumbers.Add(inputValue);
+                }
+            }
+            
+            // sort values
+            primeNumbers.Sort();
+            primeNumbers.Reverse();
+
+            nonPrimeNumbers.Sort();
+            nonPrimeNumbers.Reverse();
+
+            // print values
+            Console.WriteLine("\nAsal sayılar:");
+            foreach (var item in primeNumbers)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Asal olmayan sayılar:");
+            foreach (var item in nonPrimeNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"\nAsal sayıların sayısı: {primeNumbers.Count}");
+            Console.WriteLine($"Asal sayıların ortalaması: {primeNumbersSum / (float)primeNumbers.Count} ");
+
+            Console.WriteLine($"Asal olmayan sayıların sayısı: {nonPrimeNumbers.Count}");
+            Console.WriteLine($"Asal olmayan sayıların ortalaması: {nonPrimeNumbersSum / (float)nonPrimeNumbers.Count} ");
+        }
+    }
+}
---
+++ Samples/Assignments - 2/Assignment - 3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Assignment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputValue = Console.ReadLine();
+            char[] chrArray = inputValue.ToCharArray();
+
+            for (int m = 0; m < chrArray.Length; m++)
+            {
+                for (int i = 0; i < chrArray.Length; i++)
+                {
+                    while (chrArray[i] == 'A' || chrArray[i] == 'a')
+                    {
+                        Console.Write(chrArray[i]);
+                        chrArray[i] = ' ';
+                    }
+                }
+                for (int i = 0; i < chrArray.Length; i++)
+                {
+                    while (chrArray[i] == 'E' || chrArray[i] == 'e')
+                    {
+                        Console.Write(chrArray[i]);
+                        chrArray[i] = ' ';
+                    }
+                }
+                for (int i = 0; i < chrArray.Length; i++)
+                {
+                    while (chrArray[i] == 'I' || chrArray[i] == 'ı')
+                    {
+                        Console.Write(chrArray[i]);
+                        chrArray[i] = ' ';
+                    }
+                }
+                for (int i = 0; i < chrArray.Length; i++)
+                {
+                    while (chrArray[i] == 'İ' || chrArray[i] == 'i')
+                    {
+                        Console.Write(chrArray[i]);
+                        chrArray[i] = ' ';
+                    }
+                }
+                for (int i = 0; i < chrArray.Length; i++)
+                {
+                    while (chrArray[i] == 'O' || chrArray[i] == 'o')
+                    {
+                        Console.Write(chrArray[i]);
+                        chrArray[i] = ' ';
+                    }
+                }
+                for (int i = 0; i < chrArray.Length; i++)
+                {
+                    while (chrArray[i] == 'Ö' || chrArray[i] == 'ö')
+                    {
+                        Console.Write(chrArray[i]);
+                        chrArray[i] = ' ';
+                    }
+                }
+                for (int i = 0; i < chrArray.Length; i++)
+                {
+                    while (chrArray[i] == 'U' || chrArray[i] == 'u')
+                    {
+                        Console.Write(chrArray[i]);
+                        chrArray[i] = ' ';
+                    }
+                }
+                for (int i = 0; i < chrArray.Length; i++)
+                {
+                    while (chrArray[i] == 'Ü' || chrArray[i] == 'ü')
+                    {
+                        Console.Write(chrArray[i]);
+                        chrArray[i] = ' ';
+                    }
+                }
+            }
+        }
+    }
+}
