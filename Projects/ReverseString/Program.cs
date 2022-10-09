90247485aaab35775681b4d1d32fedcd4cfd6888
Ahmet Yağız <ahmetyagiz000@gmail.com>
2022-10-09 19:11:26 +0300

    ReverseString

---
+++ Projects/ReverseString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            Console.Write("Metni giriniz: ");
+            string input = Console.ReadLine();
+
+            // separate words
+            string[] inputSpltArr = input.Split(' ');
+
+            for (int i = 0; i < inputSpltArr.Length; i++)
+            {
+                // convert word to char array
+                Char[] charArray = inputSpltArr[i].ToCharArray();
+
+                // reverse the char array
+                Array.Reverse(charArray);
+
+                for (int j = 0; j < charArray.Length; j++)
+                {
+                    // print letters
+                    Console.Write(charArray[j]);
+                }
+
+                // space after word
+                Console.Write(" ");
+            }
+        }
+    }
+}
