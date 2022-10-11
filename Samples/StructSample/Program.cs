2e58ed41359a615d99c51a6820e0579ebefe13b4
Ahmet Yağız <ahmetyagiz000@gmail.com>
2022-10-11 22:08:00 +0300

    StructSample

---
+++ Samples/StructSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace StructSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.shortSide = 3;
+            rectangle.longSide = 4;
+
+            Console.WriteLine("The area of the rectangle is: {0}", rectangle.CalculateAreOfRectangle());
+
+            //Rectangle_Struct rectangle_Struct = new Rectangle_Struct();
+            Rectangle_Struct rectangle_Struct;
+            rectangle_Struct.shortSide = 3;
+            rectangle_Struct.longSide = 4;
+
+            Console.WriteLine("The area of the rectangle is: {0}", rectangle_Struct.CalculateAreOfRectangle());
+        }
+    }
+
+    class Rectangle
+    {
+        public int shortSide;
+        public int longSide;
+
+        public long CalculateAreOfRectangle()
+        {
+            return this.shortSide * this.longSide;
+        }
+    }
+
+    struct Rectangle_Struct
+    {
+        public int shortSide;
+        public int longSide;
+
+        public long CalculateAreOfRectangle()
+        {
+            return this.shortSide * this.longSide;
+        }
+    }
+}
