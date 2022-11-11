2d62111a3745a9a21be513150dc531e5cca389ee
Ahmet Yağız <ahmetanteryagiz@gmail.com>
2022-11-11 16:31:13 +0300

    Abstraction

---
+++ Object Oriented Programming (OOP)/Abstraction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Abstraction
+{
+    // Abstract class
+    abstract class Animal
+    {
+        // Abstract method (does not have a body)
+        public abstract void animalSound();
+    }
+
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig(); // Create a pig object
+            myPig.animalSound(); // Call the abstract method
+        }
+    }
+}
