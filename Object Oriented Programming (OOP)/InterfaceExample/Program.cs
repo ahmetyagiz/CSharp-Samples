3845d82afda446510b6402edb2d08549d5043752
Ahmet Yağız <ahmetanteryagiz@gmail.com>
2022-11-11 16:36:39 +0300

    InterfaceExample

---
+++ Object Oriented Programming (OOP)/InterfaceExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InterfaceExample
+{
+    // interface
+    interface  IAnimal
+    {
+        public abstract void animalSound(); // interface method (does not have a body)
+    }
+
+    // Pig "implements" the IAnimal interface
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig(); // Create a Pig object
+            myPig.animalSound();
+        }
+    }
+}
