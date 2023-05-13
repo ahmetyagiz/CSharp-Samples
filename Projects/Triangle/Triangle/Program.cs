b9cefa81d9ce0df1f004939fc62c0369d8c4f9dc
Ahmet Yağız <ahmetyagiz000@gmail.com>
2023-05-13 20:36:39 +0300

    Triangle

---
+++ Projects/Triangle/Triangle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Triangle
+{
+    internal class Program
+    {
+        static int triangleSize;
+
+        static void Main(string[] args)
+        {
+            UserInput();
+            DrawTriangle();
+        }
+
+        static void UserInput()
+        {
+            Console.Write("Enter the size of the triangle: ");
+            triangleSize = Int32.Parse(Console.ReadLine());
+        }
+
+        static void DrawTriangle()
+        {
+            for (int i = 0; i < triangleSize - 1; i++)
+            {
+                Console.Write("*");
+
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (i > 0)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            for (int i = 0; i < triangleSize; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
