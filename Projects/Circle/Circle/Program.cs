b4783e72c84b2a1e25adab3d7b0b02a1358b4bb9
Ahmet Yağız <ahmetyagiz000@gmail.com>
2023-05-14 14:05:12 +0300

    CircleDrawing

---
+++ Projects/Circle/Circle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class HollowCircleDrawing
+{
+    static void Main()
+    {
+        int radius = int.Parse(Console.ReadLine());
+
+        double aspectRatio = 2.0; // Genişlik/Yükseklik oranı
+
+        for (int y = -radius; y <= radius; y++)
+        {
+            for (int x = -radius * (int)aspectRatio; x <= radius * (int)aspectRatio; x++)
+            {
+                double distance = Math.Sqrt((x / aspectRatio) * (x / aspectRatio) + y * y);
+
+                if (distance > radius - 0.5 && distance < radius + 0.5)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
